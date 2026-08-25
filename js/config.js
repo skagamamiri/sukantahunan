2fe0e6f9fa6dc1dddbf72bd63f9e1b23517a1eea
skagamamiri <sekolah-8965@moe-dl.edu.my>
2026-08-25 22:01:57 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://ifqgstspfxagzstccsin.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ifqgstspfxagzstccsin.supabase.co";
 const SUPABASE_KEY = "sb_publishable_qL2mtbUgiReWMf8QTQPZhg_hVxFryAP";
 
 const supabaseClient = window.supabase.createClient(
