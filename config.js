ba10ea5caca87627db5444edbb25bf142d29a8cd
skagamamiri <sekolah-8965@moe-dl.edu.my>
2026-08-25 21:35:47 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ifqgstspfxagzstccsin.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_qL2mtbUgiReWMf8QTQPZhg_hVxFryAP";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
